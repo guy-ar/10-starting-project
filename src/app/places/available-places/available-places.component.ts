c15914c81c68ba36f99560d9befc7702b5551801
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-03-24 10:57:11 +0200

    section 12 - chapter 223 - using map to tranform the observer input for next

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -18,10 +19,15 @@ export class AvailablePlacesComponent implements OnInit {
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
   ngOnInit(): void {
-    const placesSub = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe(
+    const placesSub = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
+    .pipe(
+      // for demo purpose use map to convert response to places[]
+      map((responseDate) => responseDate.places)
+    )
+    .subscribe(
       {
-        next: (response) => {
-          console.log(response.places);
+        next: (places) => {
+          this.places.set(places);
         }
       }
     );
